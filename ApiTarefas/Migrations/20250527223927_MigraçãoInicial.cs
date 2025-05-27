12e2151ddb739c633fbfcc060516a1cef5ecb8ea
PedroHenrique1s <Phsouzahd15@gmail.com>
2025-05-27 19:41:07 -0300

    database(BancoDeDadosSql) - Realizado migração do banco de dados com sucesso

---
+++ ApiTarefas/Migrations/20250527223927_MigraçãoInicial.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiTarefas.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigraçãoInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TabelaUsuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TabelaUsuario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TabelaTarefas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TabelaTarefas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TabelaTarefas_TabelaUsuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "TabelaUsuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TabelaTarefas_UsuarioId",
+                table: "TabelaTarefas",
+                column: "UsuarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TabelaTarefas");
+
+            migrationBuilder.DropTable(
+                name: "TabelaUsuario");
+        }
+    }
+}
