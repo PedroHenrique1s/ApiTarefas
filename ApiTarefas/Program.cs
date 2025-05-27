06f13ee1db0e6e51ad0a073fc9b52e987b2acacf
PedroHenrique1s <Phsouzahd15@gmail.com>
2025-05-27 19:43:03 -0300

    configuration(program.cs)- Desenvolvimento do cors e tratamento de erros

--- ApiTarefas/Program.cs
+++ ApiTarefas/Program.cs
@@ -1,8 +1,27 @@
 using ApiTarefas.Contexto;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().ConfigureApiBehaviorOptions(
+    options =>
+    {
+        options.SuppressMapClientErrors = true;
+        options.InvalidModelStateResponseFactory = context =>
+        {
+            return new BadRequestObjectResult(new
+            {
+                Mensagem = "Preencha todos os campos corretamente",
+                Erros = context.ModelState.Where(x => x.Value!.Errors.Count > 0).ToDictionary(
+                  e => e.Key,
+                  e => e.Value!.Errors[0].ErrorMessage
+                )
+            });
+        };
+    }
+);
 
 // Add services to the container.
 
@@ -12,8 +31,27 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<BancoDados>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
 );
-
+builder.Services.AddCors(
+    options => options.AddDefaultPolicy(
+        policy => policy.SetIsOriginAllowed(origin => origin == "http://localhost:4200" || origin is null).AllowAnyMethod().AllowAnyHeader()
+    )
+);
 var app = builder.Build();
+
+app.UseCors();
+app.UseRouting();
+
+app.UseStatusCodePages(
+    async context => 
+    {
+        var response = context.HttpContext.Response;
+        if(response.StatusCode == 404 && !response.HasStarted)
+        {
+            response.ContentType = "application/json";
+            await response.WriteAsJsonAsync(new{Mensagem = "Nenhum registro encontrado"});
+        }
+    }
+);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
