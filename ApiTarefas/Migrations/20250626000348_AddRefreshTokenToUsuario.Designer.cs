19362d5dcbf89c4cbe66769c1037d76fb2d2a724
PedroHenrique1s <Phsouzahd15@gmail.com>
2025-06-25 21:46:28 -0300

    v.1.0- Feat(Migrations)- Realizado novas funcionalidades como refreshToken

--- ApiTarefas/Migrations/20250626000348_AddRefreshTokenToUsuario.Designer.cs
+++ ApiTarefas/Migrations/20250626000348_AddRefreshTokenToUsuario.Designer.cs
@@ -4,6 +4,7 @@ using ApiTarefas.Contexto;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ApiTarefas.Migrations
 {
     [DbContext(typeof(BancoDados))]
-    partial class BancoDadosModelSnapshot : ModelSnapshot
+    [Migration("20250626000348_AddRefreshTokenToUsuario")]
+    partial class AddRefreshTokenToUsuario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ApiTarefas/Migrations/20250626000348_AddRefreshTokenToUsuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiTarefas.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokenToUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "TabelaUsuario",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiracao",
+                table: "TabelaUsuario",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "TabelaUsuario");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiracao",
+                table: "TabelaUsuario");
+        }
+    }
+}
